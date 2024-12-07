e7d26e49af34527300a20a87631277750a74aed4
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-12-07 15:23:12 -0300

    Complete 3 days of AOC 2024

--- Program.cs
+++ Program.cs
@@ -1,20 +1,4 @@
-﻿// var helper = new PagnationHelper<char>(new List<char> { 'a', 'b', 'c', 'd', 'e', 'f' }, 4);
-// Console.WriteLine(helper.PageCount); //should == 2
-// Console.WriteLine(helper.ItemCount); //should == 6
-// Console.WriteLine(helper.PageItemCount(0)); //should == 4
-// Console.WriteLine(helper.PageItemCount(1)); // last page - should == 2
-// Console.WriteLine(helper.PageItemCount(2)); // should == -1 since the page is invalid
+﻿
+var aoc2024 = new AdventOfCode2024Day3();
 
-
-// Console.WriteLine(helper.PageIndex(5)); //should == 1 (zero based index)
-// Console.WriteLine(helper.PageIndex(2)); //should == 0
-// Console.WriteLine(helper.PageIndex(20)); //should == -1
-// Console.WriteLine(helper.PageIndex(-10)); //should == -1
-
-var text2 = TopWords.Top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e");
-
-foreach (var item in text2)
-{
-    Console.WriteLine(item);
-}
-
+Console.WriteLine(aoc2024.Run());
